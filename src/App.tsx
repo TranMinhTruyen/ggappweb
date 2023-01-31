90c780c8fba986bebf67624f6411d86e66a7a1c8
1607VN185\minh-truyen <minh-truyen@system-exe.com.vn>
2023-01-31 17:26:05 +0700

    add demo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,197 @@
-import React, {useEffect} from 'react';
-import './App.css';
+import * as React from 'react';
+import {SyntheticEvent, useState} from 'react';
+import {CSSObject, styled, Theme, useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import {Container, Tab, Tabs} from "@mui/material";
+import Mail from "./Mail";
+import PhoneIcon from '@mui/icons-material/Phone';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 
-import {useAppSelector, useAppDispatch} from "./redux/hooks";
-import {setToken, TokenState} from "./redux/slices/tokenSlice";
-import {Button, Container, Grid} from "@mui/material";
+const drawerWidth = 240;
 
-const App = () => {
-	const token = useAppSelector(state => state.tokenSlice);
-	const dispatch = useAppDispatch();
-	const initToken: TokenState = {
-		token: '',
-		role: '',
-		authorities: []
-	}
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
 
-	useEffect(() => {
-		console.log('Token object:', token);
-	}, [token]);
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
 
-	const testSetToken = () => {
-		initToken.token = 'test token';
-		initToken.role = 'ROLE_ADMIN';
-		initToken.authorities = ['CREATED', 'DELETED'];
+const DrawerHeader = styled('div')(({theme}) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
 
-		dispatch(setToken(initToken));
-	}
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
 
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({theme, open}) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+interface TabPanelProps {
+	children?: React.ReactNode;
+	index: number;
+	value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+	const { children, value, index, ...other } = props;
 
 	return (
-		<div className={"App"}>
-			<Container fixed={true}>
-				<Button variant="contained" onClick={() => {testSetToken()}}>Test button</Button>
-			</Container>
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`vertical-tabpanel-${index}`}
+			aria-labelledby={`vertical-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
 		</div>
 	);
 }
 
-export default App;
+function a11yProps(index: number) {
+	return {
+		id: `vertical-tab-${index}`,
+		'aria-controls': `vertical-tabpanel-${index}`,
+	};
+}
+
+export default function App() {
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+	const [value, setValue] = useState(0);
+
+	const handleChange = (event: SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && {display: 'none'}),
+                        }}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Mini variant drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                    </IconButton>
+                </DrawerHeader>
+				<Tabs orientation="vertical"
+					  variant="scrollable"
+					  value={value}
+					  onChange={handleChange}
+				>
+					<Tab icon={<PhoneIcon />} iconPosition="start" label={ open ? "Item One" : ""} {...a11yProps(0)}/>
+					<Tab icon={<FavoriteIcon />} iconPosition="start" label={ open ? "Item Two" : ""} {...a11yProps(1)}/>
+					<Tab icon={<PersonPinIcon />} iconPosition="start" label={ open ? "Item Three" : ""} {...a11yProps(2)}/>
+				</Tabs>
+            </Drawer>
+			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+				<DrawerHeader />
+				<TabPanel value={value} index={0}>
+					<Mail/>
+				</TabPanel>
+				<TabPanel value={value} index={1}>
+					<Container>
+						<Typography>Item 1</Typography>
+					</Container>
+				</TabPanel>
+				<TabPanel value={value} index={2}>
+					<Container>
+						<Typography>Item 2</Typography>
+					</Container>
+				</TabPanel>
+			</Box>
+        </Box>
+    );
+}
